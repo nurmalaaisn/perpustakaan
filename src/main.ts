7ab70956caa8ce1e75fd9540cf41c206819cdbec
nurmalaaisn <nurmalaaisn@gmail.com>
2026-03-12 10:56:23 +0700

    fix: listen on 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-  console.log(`App running on: http://localhost:${process.env.PORT ?? 3000}/api`);
+  console.log(`App running on port: ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
